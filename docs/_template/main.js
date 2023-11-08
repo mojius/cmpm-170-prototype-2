87ceca13163ae2a099d9717e91045ea1759c0d38
Abel Goy <agoy@ucsc.edu>
2023-11-08 13:57:51 -0800

    Basic movement

--- docs/_template/main.js
+++ docs/_template/main.js
@@ -1,13 +1,125 @@
-title = "";
+// The title of the game to be displayed on the title screen
+title = "CHARGE RUSH";
 
+// The description, which is also displayed on the title screen
 description = `
 `;
 
-characters = [];
+// The array of custom sprites
+characters = [
+`
+  ll
+  ll
+ccllcc
+ccllcc
+ccllcc
+cc  cc
+`
+];
 
-options = {};
+// Game design variable container
+const G = {
+	WIDTH: 150,
+	HEIGHT: 100,
 
+    STAR_SPEED_MIN: 0.5,
+	STAR_SPEED_MAX: 1.0
+};
+
+// Game runtime options
+// Refer to the official documentation for all available options
+options = {
+	theme: "dark",
+	viewSize: {x: G.WIDTH, y: G.HEIGHT},
+    isCapturing: true,
+    isCapturingGameCanvasOnly: true,
+    captureCanvasScale: 2
+};
+
+// JSDoc comments for typing
+/**
+ * @typedef {{
+ * pos: Vector,
+ * speed: number
+ * }} Star
+ */
+
+/**
+ * @type { Star [] }
+ */
+let stars;
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * }} Player
+ */
+
+/**
+ * @type { Player }
+ */
+let player;
+let shipY = 80;
+let shipDown = 0;
+// The game loop function
 function update() {
-  if (!ticks) {
-  }
+    // The init function running at startup
+	if (!ticks) {
+        // A CrispGameLib function
+        // First argument (number): number of times to run the second argument
+        // Second argument (function): a function that returns an object. This
+        // object is then added to an array. This array will eventually be
+        // returned as output of the times() function.
+		stars = times(20, () => {
+            // Random number generator function
+            // rnd( min, max )
+            const posX = rnd(0, G.WIDTH);
+            const posY = rnd(0, G.HEIGHT);
+            // An object of type Star with appropriate properties
+            return {
+                // Creates a Vector
+                pos: vec(posX, posY),
+                // More RNG
+                speed: rnd(G.STAR_SPEED_MIN, G.STAR_SPEED_MAX)
+            };
+        });
+
+        player = {
+            //pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5)
+			pos: vec(10, 10)
+
+        };
+	}
+
+    // Update for Star
+    stars.forEach((s) => {
+        // Move the star downwards
+        s.pos.y += s.speed;
+        // Bring the star back to top once it's past the bottom of the screen
+        if (s.pos.y > G.HEIGHT) s.pos.y = 0;
+
+        // Choose a color to draw
+        color("light_black");
+        // Draw the star as a square of size 1
+        box(s.pos, 1);
+    });
+	if (shipDown == 0) {
+		shipY++;
+	} else if (shipDown == 1) {
+		shipY--;
+	}
+	if (shipY == 95) {
+		shipDown = 1;
+	} else if (shipY == 20) {
+		shipDown = 0;
+	}
+    player.pos = vec(10, shipY);
+	//Go back and forth between 20 and 140
+	console.log(input.pos.y);
+    player.pos.clamp(0, G.WIDTH, 0, G.HEIGHT);
+
+    // color("cyan");
+    color ("light_black");
+    // box(player.pos, 4);
+    char("a", player.pos);
 }
